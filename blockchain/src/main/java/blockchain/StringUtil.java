c5c6d3d042fdcc55e3b215a6d25dba221bf0e778
Sarfraz Anwar <developer.sarfraz@gmail.com>
2019-04-29 12:44:43 +0000

    completed blockchain

--- blockchain/src/main/java/blockchain/StringUtil.java
+++ blockchain/src/main/java/blockchain/StringUtil.java
@@ -1,26 +1,25 @@
 package blockchain;
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.*;
 
 public class StringUtil{
-    //apply SHA256 to a string and return it
-    public static String applySha256(String input){
-        try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            //apply sha256
-            byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for(int i=0; i< hash.length; i++){
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        }
-        catch(Exception e){
-            throw new RuntimeException(e);
-        }
+    //Applies Sha256 to a string and returns the result.
+	public static String applySha256(String input){
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			//Applies sha256 to our input,
+			byte[] hash = digest.digest(input.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
     }
 
     //Applies ECDSA Signature and returns the result ( as bytes ).
@@ -40,7 +39,7 @@ public class StringUtil{
 		return output;
 	}
 
-    //Verifies a String signature
+	//Verifies a String signature
 	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
 		try {
 			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
@@ -52,7 +51,32 @@ public class StringUtil{
 		}
 	}
 
-    public static String getStringFromKey(Key key) {
+	public static String getStringFromKey(Key key) {
 		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
+
+    //Tacks in array of transactions and returns a merkle root.
+public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		ArrayList<String> previousTreeLayer = new ArrayList<String>();
+		for(Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		ArrayList<String> treeLayer = previousTreeLayer;
+		while(count > 1) {
+			treeLayer = new ArrayList<String>();
+			for(int i=1; i < previousTreeLayer.size(); i++) {
+				treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+	}
+
+    public static String getDificultyString(int difficulty){
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        return target;
+    }
 }
